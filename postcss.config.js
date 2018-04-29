dae7415dec781cea8de84a944e8e1804828518af
ExE Boss <ExE-Boss@users.noreply.github.com>
2018-04-29 20:20:00 +0200

    Modularise the project

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = (ctx) => ({
 	map:	ctx.options.map,
 	parser:	ctx.file.extname === '.sss' ? 'sugarss' : false,
 	plugins: {
+		'postcss-import': { root: ctx.file.dirname },
 		'autoprefixer': { root: ctx.file.dirname },
 	}
 });
