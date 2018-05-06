95f4cecec83011cd1225ec550522ed3b4bc5f961
ExE Boss <ExE-Boss@users.noreply.github.com>
2018-05-06 12:45:00 +0200

    Use BrowserSync to implement the `serve` task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp	= require("gulp");
 const postcss	= require("gulp-postcss");
 const sourcemaps	= require("gulp-sourcemaps");
+const browserSync	= require("browser-sync");
 
 const autoprefixer	= require("autoprefixer");
 const postcssImport	= require("postcss-import");
@@ -24,5 +25,27 @@ gulp.task("post-layout", () => {
 			]
 		})))
 		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(args.dest || "./dist/"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task("post-layout-html", () => {
+	const args = argsBuilder.argv;
+
+	return gulp.src("./src/post-layout/smw-central.html")
 		.pipe(gulp.dest(args.dest || "./dist/"));
+});
+
+gulp.task("serve", ["post-layout", "post-layout-html"], () => {
+	gulp.src("./src/post-layout/smw-central.html")
+		.pipe(gulp.dest("./dist"));
+
+	browserSync.init({
+		open: false,
+		server: "./dist"
+	});
+
+	gulp.watch("./src/post-layout/*.css",	["post-layout"]);
+	gulp.watch("./src/post-layout/*.html",	["post-layout-html"])
+		.on("change", browserSync.reload);
 });
